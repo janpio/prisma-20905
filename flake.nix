5680d0d957bf7cb8744325541226678607416a13
Haak Saxberg <haak.erling@gmail.com>
2023-08-31 16:23:28 -0700

    Switch from sqlite to postgres

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Repro conditions for prisma/prisma#20905";
+  description = "Kept webapp experience";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -17,8 +17,32 @@
         devShell = with pkgs; mkShell {
           packages = [
             nodejs-18_x
-            sqlite
+            postgresql_15
           ];
+
+          shellHook = ''
+            export PGDIR=$(pwd)/.direnv/.postgres
+            export PGHOST=$PGDIR
+            export PGDATA=$PGDIR/data
+            export PGLOG=$PGDIR/log
+
+            if test ! -d $PGDIR; then
+              mkdir -p $PGDIR
+            fi
+
+            if [ ! -d $PGDATA ]; then
+              echo 'Initializing postgresql database...'
+              initdb $PGDATA --auth=trust >/dev/null
+
+              cat <<EOF >>"$PGDATA/postgresql.conf"
+            listen_addresses = 'localhost'
+            unix_socket_directories = '$PGHOST'
+            EOF
+
+              echo "CREATE USER admin WITH PASSWORD 'password' SUPERUSER" | postgres --single -E postgres
+              echo "CREATE DATABASE webapp_db OWNER admin" | postgres --single -E postgres
+            fi
+          '';
         };
       });
 }
